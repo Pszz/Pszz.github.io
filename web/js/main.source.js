908ba46d68b41af6d3952b8ec42ba94ed59b6319
Pszz <pi951357@qq.com>
2015-08-25 18:53:36 +0800

    add canvas filter

--- web/js/main.source.js
+++ web/js/main.source.js
@@ -8,19 +8,40 @@
 void function(){
 	var api = API;
    //返回顶部
-  var scrolldelay;
-  api.PgUp = function(flag){
-     // window.scrollTo(0,0); 直接回顶部
-     //暂时不使用动画
-     var y = window.scrollY;
-     if(y <= 0 || flag){
-        clearTimeout(scrolldelay);
-     }else{
-        window.scrollBy(0,-100); 
-        scrolldelay = setTimeout(function(){
-        	$.PgUp();
-		},10); 
-    }
-  };
+	void function(){
+		  var scrolldelay;
+		  api.PgUp = function(flag){
+			 // window.scrollTo(0,0); 直接回顶部
+			 //暂时不使用动画
+			 var y = window.scrollY;
+			 if(y <= 0 || flag){
+				clearTimeout(scrolldelay);
+			 }else{
+				window.scrollBy(0,-100); 
+				scrolldelay = setTimeout(function(){
+					$.PgUp();
+				},10); 
+			}
+		  };
+	}();
+  //弹窗
+  void function(fm){
+	  
+	 var mask = [
+	 	'<div class="mask">',
+		 	'{#data}',
+		'</div>'
+	 ].join("");
+	  
+	//alert; - msg
+  	fm.alert = function(s){
+
+	}; 
+	//confirm  - msg ,yes ,no
+	fm.confirm = function(s,y,n){
+		
+	};
+  }(api.msg = {});
+
 }; 
 
